12be567b06b3ed89d790a655c71e0f43bee8cbc7
Reto Ryter <reto@twy.gmbh>
2020-10-21 09:00:41 +0200

    chore(cleanup): remove obsolete modal

--- apps/rtm/src/app/erc725/erc725.module.ts
+++ apps/rtm/src/app/erc725/erc725.module.ts
@@ -12,12 +12,11 @@ import { ProxyAccountComponent } from './proxy-account/proxy-account.component';
 import { ProgressComponent } from './progress/progress.component';
 import { NewAccountComponent } from './new-account/new-account.component';
 import { LoadingComponent } from './loading/loading.component';
-import { ModalComponent } from './modal/modal.component';
 import { AddKeyComponent } from './key-manager/add-key/add-key.component';
 import { PriviligesComponent } from './key-manager/priviliges/priviliges.component';
 
 const routes: Routes = [
-  { path: '', component: Erc725Component },
+  { path: '', component: NewAccountComponent },
   { path: 'create-wallet', component: NewAccountComponent },
   {
     path: 'accounts/:address',
@@ -36,7 +35,6 @@ const routes: Routes = [
     ProgressComponent,
     NewAccountComponent,
     LoadingComponent,
-    ModalComponent,
     AddKeyComponent,
     PriviligesComponent,
   ],
