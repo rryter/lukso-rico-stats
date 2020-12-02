04f1ffb2b4daebe5afddd48e7474af64229e265a
Reto Ryter <reto@twy.gmbh>
2020-12-02 16:13:17 +0100

    chore(e2e): fix test

--- apps/rtm-e2e/src/integration/app.spec.ts
+++ apps/rtm-e2e/src/integration/app.spec.ts
@@ -16,10 +16,7 @@ describe('ERC 725 Home', () => {
     cy.getByAttr('amount__accept').click();
     cy.getByAttr('wallet__value').contains(100);
     cy.getByAttr('overview-account__go-to-account-detail').click();
-    cy.getByAttr('privileges__keys')
-      .contains('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266')
-      .parent()
-      .contains('Management');
+    cy.getByAttr('privileges__keys').contains('b92266').parent().contains('Management');
     cy.getByAttr('layout__nav__home').click();
     cy.getByAttr('wallet__withdraw').click();
     cy.getByAttr('amount__value').type('100', { force: true });
@@ -30,9 +27,6 @@ describe('ERC 725 Home', () => {
     cy.getByAttr('keymanager__add-key').click();
     cy.getByAttr('add-key__address').type('0x70997970C51812dc3A010C7d01b50e0d17dc79C8');
     cy.getByAttr('add-key__confirm').click();
-    cy.getByAttr('privileges__keys')
-      .contains('0x70997970C51812dc3A010C7d01b50e0d17dc79C8')
-      .parent()
-      .contains('Execution');
+    cy.getByAttr('privileges__keys').contains('dc79C8').parent().contains('Execution');
   });
 });
--- apps/rtm/src/app/accounts/overview/overview-claims/overview-claims.component.ts
+++ apps/rtm/src/app/accounts/overview/overview-claims/overview-claims.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lukso-overview-claims',
   templateUrl: './overview-claims.component.html',
-  styleUrls: ['./overview-claims.component.scss']
+  styleUrls: ['./overview-claims.component.scss'],
 })
 export class OverviewClaimsComponent implements OnInit {
+  @Input() showAddButton = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
